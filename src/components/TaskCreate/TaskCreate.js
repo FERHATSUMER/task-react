1af0ae7b1404936d1a128bad38f8546ad73d99b3
FERHATSUMER <sumerisa5@gmail.com>
2023-05-30 18:03:03 +0300

    task-react

---
+++ src/components/TaskCreate/TaskCreate.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import '../../Styles/TaskCreate.css';
+
+function TaskCreate() {
+  const [title, setTitle] = useState('');
+  const [taskDec, setTaskDec] = useState('');
+
+  console.log(title, taskDec);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const hanleTaskChange = (event) => {
+    setTaskDec(event.target.value);
+  };
+ 
+  const handleSubmit=(event)=>{
+event.preventDefault()
+  }
+
+  return (
+    <div className='task-create'>
+      <h3>Lütfen Task Ekleyiniz</h3>
+      <form className='task-form'>
+        <label className='task-label'>Başlık</label>
+        <input className='task-input' value={title} onChange={handleChange} />
+        <label className='task-label'>Task Giriniz</label>
+        <textarea
+          className='task-input'
+          rows={5}
+          value={taskDec}
+          onChange={hanleTaskChange}
+        />
+        <button className='task-button'
+        onClick={handleSubmit}
+        >Oluştur</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskCreate;
---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,7 @@
+function TaskList() {
+    return (
+    <div>
+    </div> );
+}
+
+export default TaskList;
