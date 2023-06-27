4dba32713fa29ea7e1bafab7eeb0d278660f1661
FERHATSUMER <sumerf39@gmail.com>
2023-06-27 23:52:03 +0300

    task-react

--- src/components/TaskShow/taskShow.js
+++ src/components/TaskShow/taskShow.js
@@ -1,6 +1,15 @@
 function TaskShow({ task }) {
-    console.log(task);
-    return (<div></div>  );
+    return (<div>
+        <h3>Görevleriniz</h3>
+        <p>{task.title}</p>
+        <h3>Yapılacaklar</h3>
+        <p>{task.taskDec}</p>
+        <div>
+            <button>Sil</button>
+            <button>Güncelle</button>
+        </div>
+
+    </div>  );
 }
 
 export default TaskShow;
